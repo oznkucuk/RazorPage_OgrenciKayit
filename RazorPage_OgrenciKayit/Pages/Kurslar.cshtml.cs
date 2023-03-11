1dba063c2b51c0d3faf5a89330ed1a0391af6db9
ozan <oznkucuk@gmail.com>
2023-03-11 20:09:31 +0300

    ön kısım alıştırma yapıldı

---
+++ RazorPage_OgrenciKayit/Pages/Kurslar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage_OgrenciKayit.Pages
+{
+    public class KurslarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
