fcc8ef7a680f27f2324954ae66677a40745b6fa3
ozan <oznkucuk@gmail.com>
2023-03-14 11:09:53 +0300

    iletişim formuna yazılanlar veri tabanına kayır edildi. veri tabanı silinip yeniden migation yapıldı

--- RazorPage_OgrenciKayit/Migrations/20230314080615_deneme1.cs
+++ RazorPage_OgrenciKayit/Migrations/20230314080615_deneme1.cs
@@ -4,10 +4,32 @@
 
 namespace RazorPage_OgrenciKayit.Migrations
 {
-    public partial class ikincitasima : Migration
+    public partial class deneme1 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Iletisims",
+                columns: table => new
+                {
+                    MesajId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdSoyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Konu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Aciklama = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IletisimMesajId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Iletisims", x => x.MesajId);
+                    table.ForeignKey(
+                        name: "FK_Iletisims_Iletisims_IletisimMesajId",
+                        column: x => x.IletisimMesajId,
+                        principalTable: "Iletisims",
+                        principalColumn: "MesajId");
+                });
+
             migrationBuilder.CreateTable(
                 name: "Kurs",
                 columns: table => new
@@ -43,6 +65,11 @@ namespace RazorPage_OgrenciKayit.Migrations
                 });
 
             migrationBuilder.CreateIndex(
+                name: "IX_Iletisims_IletisimMesajId",
+                table: "Iletisims",
+                column: "IletisimMesajId");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_Ogrencis_KursId",
                 table: "Ogrencis",
                 column: "KursId");
@@ -50,6 +77,9 @@ namespace RazorPage_OgrenciKayit.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Iletisims");
+
             migrationBuilder.DropTable(
                 name: "Ogrencis");
 
